441bce15405a020be302bc85b385d3a92ceeb812
Zobeir <rigi.zobir@yahoo.com>
2023-04-24 02:07:36 +0100

    topbar finished

--- src/comp/TopBar.jsx
+++ src/comp/TopBar.jsx
@@ -1,19 +1,33 @@
-import "./TopBar.css"
-import React from 'react'
+import "./TopBar.css";
+import React from "react";
 
 const TopBar = () => {
   return (
     <div className="top">
       <div className="topLeft">
-        <i class="fa-brands fa-square-facebook"></i>
-        <i class="fa-brands fa-square-twitter"></i>
-        <i class="fa-brands fa-square-pinterest"></i>
-        <i class="fa-brands fa-square-instagram"></i>
+        <i className="topIcone fa-brands fa-square-facebook"></i>
+        <i className="topIcone fa-brands fa-square-twitter"></i>
+        <i className="topIcone fa-brands fa-square-pinterest"></i>
+        <i className="topIcone fa-brands fa-square-instagram"></i>
       </div>
-      <div className="topCenter">c</div>
-      <div className="topRight">r</div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImg" alt="profileImg"
+          src="https://live.staticflickr.com/65535/52399136398_35c64ec6ba_h.jpg"
+        />
+        <i className=" topSearchIcon fa-solid fa-magnifying-glass"></i>{" "}
+      </div>
     </div>
   );
-}
+};
 
-export default TopBar
+export default TopBar;

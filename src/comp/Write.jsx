3f4f7b70ca8b330b4ad09067d97c216e4b1263ad
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-04-30 16:32:26 +0100

    write css done

--- src/comp/Write.jsx
+++ src/comp/Write.jsx
@@ -3,10 +3,15 @@ import "../styles/Write.css";
 function Write() {
   return (
     <div className="write">
+      <img
+        className="writeImg"
+        src="https://i0.wp.com/www.languageonthemove.com/wp-content/uploads/2022/10/wlf47.jpg?resize=600%2C450&ssl=1"
+        alt=""
+      />
       <form className="writeForm">
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
-            <i class="fa-solid fa-plus"></i>
+            <i className="writeIcon fa-solid fa-plus"></i>
           </label>
           <input type="file" id="fileInput" style={{ display: "none" }} />
           <input

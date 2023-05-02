55fce3242f3fa1c2d67391652464e2d029557c06
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-05-02 02:11:33 +0100

    register jsx & css done

--- src/comp/Register.jsx
+++ src/comp/Register.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import "../styles/Register.css"
+
 
 const Register = () => {
   return (
-    <div className='register'>
-    Register
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>UserName</label>
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter you'r UserName..."
+        />
+        <label>Email</label>
+        <input
+          className="registerInput"
+          type="email"
+          placeholder="Enter you'r email..."
+        />
+        <label>Password</label>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter you'r password..."
+        />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;

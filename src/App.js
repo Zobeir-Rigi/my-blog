9edfbe5d07aee05fb50b2dae9e3e3b1e215cb045
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-05-02 02:42:31 +0100

    routing the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import Home from "./comp/Home"
+import Write from "./comp/Write"
+import Login from "./comp/Login"
 import Register from "./comp/Register";
+import Settings from "./comp/Settings"
+import Single from "./comp/Single"
 import TopBar from "./comp/TopBar";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <TopBar />
-      <Register />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
